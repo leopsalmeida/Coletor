fcf54195ce09dbb87acb82a2ae0d2526d39838df
Leonardo Almeida <leopsalmeida@gmail.com>
2015-07-02 15:32:47 -0300

    1st Commit

---
+++ app/src/main/java/com/example/pauer/coletorandroid/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.example.pauer.coletorandroid;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class SplashActivity extends ActionBarActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        abrirHome();
+    }
+
+
+    private void abrirHome() {
+
+
+        Handler handler = new Handler();
+
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
